32f21a157cc32dac55a19a634ccf82f7ae3d588e
Takeshi Matsubayashi <takeshimatsubayashi@Takeshis-MacBook-Air.local>
2022-12-09 22:50:36 +0900

    adding project files

---
+++ data/process_data.py
@@ -0,0 +1,108 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """This function loads and merge datasets.
+    
+    Params:
+        messages_filepath: messages filepath
+        categories_filepath: categories filepath
+    
+    Return:
+        df: merged dataframe
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = pd.merge(messages, categories, on='id', how='left')
+    
+    return df
+
+
+def clean_data(df):
+    """This function is for cleaning data before being pushed to a database
+    
+    Params:
+        df: merged dataframe
+    Return:
+        df: merged dataframe
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str.slice(start=-1)
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # Replace categories column in df with new category columns.
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1, sort=False)
+    
+    df['related'].replace(2,1,inplace=True)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
+
+
+def save_data(df, database_filename):
+    """This function saves the clean dataset into an sqlite database.
+    
+    Params:
+        df: cleaned dataframe
+        database_filename: database filename
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('InsertTableName', engine, index=False, if_exists='replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,149 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+import pickle
+
+def load_data(database_filepath):
+    """This function loads data from a database
+       and create 2 aliases for building a ML model.
+    
+    Params:
+        database_filepath: database filepath
+    
+    Return:
+        X: feature
+        Y: target variable
+    """
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('InsertTableName', engine)
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns
+    
+    return X, Y, category_names
+
+
+def tokenize(text):
+    """This function returns list of cleaned words
+    after 4 steps below, normalize, tokenize, remove stopwords, and lemmatize.
+    
+    Params:
+        text: text
+    Return:
+        lemmed: list of cleaned words
+    """
+    # normalize
+    normalized_text = re.sub(r'[^a-zA-Z0-9]', " ", text.lower())
+    
+    # tokenize
+    words = word_tokenize(normalized_text)
+    
+    # remove stopwords
+    words = [w for w in words if w not in stopwords.words('english')]
+    
+    # stem / lemmatize
+    # reduce words to their stems
+    stemmed = [PorterStemmer().stem(w) for w in words]
+    
+    # reduce words to their root form using default pos
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in stemmed]
+    
+    return lemmed
+
+
+def build_model():
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+    #'clf__estimator__bootstrap': True,
+    #'vect__max_df': 0.5,
+    'clf__estimator__n_estimators': [10],
+    'clf__estimator__min_samples_split': [2],
+    }
+
+    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=5, verbose=2, cv=2)
+    
+    return model
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """This function evaluates a model.
+    
+    Params:
+        model: ML model developed
+        X_test: X for test
+        Y_test: Y for test
+        category_names: 
+    """
+    y_pred = model.predict(X_test)
+    
+    print(classification_report(Y_test, y_pred, target_names=category_names))
+
+
+def save_model(model, model_filepath):
+    """This function is for saving a model.
+    
+    Params:
+        model: ML model developed
+        model_filepath: model filepath
+    """
+    file = open(model_filepath, 'wb')
+    pickle.dump(model, file)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
